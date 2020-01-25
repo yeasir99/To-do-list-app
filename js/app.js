392bf1cd2e5dd20e1cf89fd11262d003af965434
yeasirahamed <yahamed007@gmail.com>
2020-01-25 13:58:59 +0600

    add button functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 function UiControlar() {};
 UiControlar.prototype.addTaskToList = function (task) {
+    if (task.value === '') {
+        alert(`Before submit please inter task name`);
+        return;
+    }
     const container = document.querySelector('.task-container')
     const div = document.createElement('div');
     div.className = 'task-container_item'
@@ -19,12 +23,50 @@ UiControlar.prototype.addTaskToList = function (task) {
     task.value = '';
 
 };
+
+UiControlar.prototype.editTask = function (e) {
+    const taskList = e.target.parentElement.parentElement;
+    const taskName = taskList.firstElementChild;
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = taskName.textContent;
+    taskList.insertBefore(input, taskName);
+    taskList.removeChild(taskName);
+    input.className = 'item-name';
+    e.target.textContent = 'Save';
+};
+
+UiControlar.prototype.saveTask = function (e) {
+    const taskList = e.target.parentElement.parentElement;
+    const input = taskList.firstElementChild;
+    const div = document.createElement('div');
+    div.textContent = input.value;
+    taskList.insertBefore(div, input);
+    taskList.removeChild(input);
+    div.className = 'item-name';
+    e.target.textContent = 'Edit';
+}
+
+UiControlar.prototype.removeTask = function (e) {
+    const taskList = e.target.parentElement.parentElement;
+    taskList.remove();
+}
+
+
 document.querySelector('form').addEventListener('submit', function (e) {
     const task = document.querySelector('input[type=text]');
-    if (task.value !== '') {
-        var ui = new UiControlar;
-        ui.addTaskToList(task);
-    }
+    var ui = new UiControlar;
+    ui.addTaskToList(task);
     e.preventDefault();
-
 });
+
+document.querySelector('.task-container').addEventListener('click', function (e) {
+    var ui = new UiControlar;
+    if (e.target.textContent === 'Edit') {
+        ui.editTask(e);
+    } else if (e.target.textContent === 'Save') {
+        ui.saveTask(e);
+    } else if (e.target.textContent === 'Remove') {
+        ui.removeTask(e);
+    }
+})
